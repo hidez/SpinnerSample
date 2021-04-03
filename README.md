796a2ef51d5e431e7c5fc553975e4c6c4e5391d0
Hidehisa Yokoyama <hidehisa@blowbend.jp>
2021-04-03 11:41:26 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# SpinnerSample
+This is spinner sample code for Android by Kotlin
+This sample code is written for my blog(https://phosphor-bronze.blogspot.com/).
+
+# License
+The MIT License
+See the LICENCE.
---
+++ app/src/main/java/bar/foo/spinnersample/MainActivity.kt
@@ -0,0 +1,11 @@
+package bar.foo.spinnersample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
